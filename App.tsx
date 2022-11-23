114b3ed67e7f63cbc188ee808c53ea191d5d316d
Emerson <pduarte.emerson@gmail.com>
2022-11-23 11:05:58 -0300

    adjusts status-bar

--- App.tsx
+++ App.tsx
@@ -3,12 +3,14 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/
 import theme from './src/theme';
 import Groups from 'screens/Groups';
 import {Loading} from "components/Loading";
+import {StatusBar} from "react-native";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
       { fontsLoaded ? <Groups/> : <Loading /> }
     </ThemeProvider>
   );
