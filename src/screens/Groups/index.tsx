15398ba189e6c52d3fc40f39e8d760e6529bfeeb
Emerson <pduarte.emerson@gmail.com>
2022-11-23 09:59:07 -0300

    initial configs and defines theme

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,9 @@
+import * as Styles from "screens/Groups/styles";
+
+export default function Groups() {
+  return (
+    <Styles.Container>
+      <Styles.Title>Groups</Styles.Title>
+    </Styles.Container>
+  );
+}
