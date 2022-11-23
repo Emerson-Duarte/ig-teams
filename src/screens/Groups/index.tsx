ad7fd418dc963e01d4952765252e5983ce131361
Emerson <pduarte.emerson@gmail.com>
2022-11-23 11:17:28 -0300

    creates Header component and creates type for png files

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,8 +1,10 @@
 import * as Styles from "screens/Groups/styles";
+import { Header } from "components/Header";
 
 export default function Groups() {
   return (
     <Styles.Container>
+      <Header />
     </Styles.Container>
   );
 }
