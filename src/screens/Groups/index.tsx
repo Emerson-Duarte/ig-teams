2f1d1c3b112101929d2c097d2f8f0c93815b3ee3
Emerson <pduarte.emerson@gmail.com>
2022-11-23 10:36:57 -0300

    creates types for theme

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -3,7 +3,6 @@ import * as Styles from "screens/Groups/styles";
 export default function Groups() {
   return (
     <Styles.Container>
-      <Styles.Title>Groups</Styles.Title>
     </Styles.Container>
   );
 }
