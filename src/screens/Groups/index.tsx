7af6a0bd7c63331fc3d36b00a5b0412e00b7a8cd
Emerson Duarte <pduarte.emerson@gmail.com>
2022-11-24 15:18:42 -0300

    creates Highlight component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,12 @@
 import * as Styles from "screens/Groups/styles";
 import { Header } from "components/Header";
+import {Highlight} from "components/Highlight";
 
 export default function Groups() {
   return (
     <Styles.Container>
       <Header />
+      <Highlight title="Título" subtitle="Subtítulo" />
     </Styles.Container>
   );
 }
