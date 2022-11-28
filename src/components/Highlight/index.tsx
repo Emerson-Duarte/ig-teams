c1ec53ae48a67483682e97f980e4219108fc8c02
Emerson <pduarte.emerson@gmail.com>
2022-11-28 10:54:56 -0300

    creates Highlight component

---
+++ src/components/Highlight/index.tsx
@@ -0,0 +1,15 @@
+import * as Styles from './styles';
+
+type Props = {
+  title: string,
+  subtitle: string
+}
+
+export function Highlight({ title, subtitle }: Props) {
+  return(
+    <Styles.Container>
+      <Styles.Title>{title}</Styles.Title>
+      <Styles.Subtitle>{subtitle}</Styles.Subtitle>
+    </Styles.Container>
+  )
+}
