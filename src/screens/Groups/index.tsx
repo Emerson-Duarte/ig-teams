d833dc989c0c6b919857560d647771dab9ee43c0
Emerson <pduarte.emerson@gmail.com>
2022-11-28 10:55:20 -0300

    updates Group screen with new components

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,18 @@
 import * as Styles from "screens/Groups/styles";
 import { Header } from "components/Header";
+import {Highlight} from "components/Highlight";
+import {GroupCard} from "components/GroupCard";
 
 export default function Groups() {
   return (
     <Styles.Container>
       <Header />
+
+      <Highlight title="Turmas" subtitle="Jogue com sua turma" />
+
+      <GroupCard title="Galera do Vakinha" />
+
+
     </Styles.Container>
   );
 }
